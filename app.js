2b6961557319779aa3da041e9121064bd4ef289b
OmarHassouna-PS <omarps552@gmail.com>
2023-03-17 06:09:36 -0700

    Add Files

---
+++ app.js
@@ -0,0 +1,77 @@
+// Q1
+let x = 5;
+alert("-9*3 = " + -9*3);
+alert("Value is " + 50);
+alert("17%5 = " + 17%5);
+alert("5%17 = " + 5%17);
+alert("5/10 = " + 5/10);
+alert(4==4);
+alert(4 != 5);
+alert(7 <= 8);
+alert(Math.ceil(x) - Math.floor(x));
+
+// Q2
+alert(prompt("Enter Number"));
+
+// Q3
+let num1 = prompt("Enter Number 1");
+let num2 = prompt("Enter Number 2");
+
+( num1 > num2 ) ? alert( num2 + " " + num1 ) : alert( num1 + " " + num2 );
+
+// Q4
+num1 = prompt("Enter Number 1");
+num2 = prompt("Enter Number 2");
+
+( num1 > num2 ) ? alert( num1 ) : alert( num2 ); 
+
+// Q5
+num1 = prompt("Enter Number 1");
+num2 = prompt("Enter Number 2");
+
+alert(Number(num1) + Number(num2));
+
+// Q6
+let num = 5;
+switch (num) {
+    case 1:
+        alert(1)
+        break;
+    case 2:
+        alert(2)
+        break;
+    case 3:
+        alert(3)
+        break;
+    case 4:
+        alert(4)
+        break;
+    case 5:
+        alert(5)
+        break;
+    case 6:
+        alert(6)
+        break;
+    case 7:
+        alert(7)
+        break;
+    case 8:
+        alert(8)
+        break;
+    case 9:
+        alert(9)
+        break;
+    default:
+        alert("PLEASE TRY AGAIN")
+        break;
+}
+
+
+
+
+
+
+
+
+
+
