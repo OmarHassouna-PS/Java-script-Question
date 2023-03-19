eb8bf6feb3f871ba23dc61d32cd45e6d786bafdc
OmarHassouna-PS <omarps552@gmail.com>
2023-03-19 06:00:15 -0700

    Updates to task 2

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ num2 = prompt("Enter Number 2");
 alert(Number(num1) + Number(num2));
 
 // Q6
-let num = 5;
-switch (num) {
+let num6 = 5;
+switch (num6) {
     case 1:
         alert(1)
         break;
@@ -68,8 +68,65 @@ switch (num) {
 
 
 
+// Q7:
+
+for (let index = 0; index <= 5; index++) {
+    alert(index);
+    
+} 
+
+// Q8:
+
+let num8 = "";
+for (let index = 0; index <= 5; index++) {
+
+    num8 += index;
+}
+alert(num8);
+
+// Q9:
+
+for (let index = 0; index < 20; index+=3) {
+        console.log(index);
+} 
+
+// Q10:
+let num10 = 0;
+num10 = prompt("Enter number of range 0 to 100");
+
+while (num10 < 0 || num10 > 100) {
+    num10 = prompt("missing input, Please Enter number of range 0 to 100");
+}
+
+// Q11:
+ 
+let num11 = 0;
+num = prompt("Enter number of range 0 to 100");
+
+while (isNaN(num)) {
+    num = prompt("missing input you most enter a number, Please Enter number of range 0 to 100");
+} 
 
 
+// Q12:
+
+let num12 = prompt("Enter a number");
+let sum12 = 0;
+for (let index = 0; index <= num12; index++) {
+    
+    sum12 += index;
+}
+alert(sum12);
+
+// Q13:
+
+let num13 = prompt("Enter a number");
+let sum13 = 0;
+for (let index = 0; index <= num13; index++) {
+    
+    sum13 += index;
+}
+alert(sum13 / (Number(num13) + 1));
 
 
 
